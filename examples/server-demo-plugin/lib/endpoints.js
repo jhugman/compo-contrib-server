36a1c9b755dac204e3c0d62ce1072d8fce82d319
James Hugman <james@hugman.tv>
2015-09-22 18:14:58 +0100

    Better demo.

--- examples/server-demo-plugin/lib/endpoints.js
+++ examples/server-demo-plugin/lib/endpoints.js
@@ -1,12 +1,5 @@
-
-
-exports.getRoot = (req, res, next) => {
-  res.send('Hello world! I\'m trapped in a plugin')
-  res.end()
-}
-
 exports.getUser = (req, res, next) => {
   console.log('hello user')
-  res.send('Hello ' + req.params.user)
+  res.send('<h1>Hello ' + req.params.user + '</h1><img src="/r/it-worked.gif">')
   res.end()
 }
